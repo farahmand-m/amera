da6e6127f3287c32097b2b1fb03389531f25220b
Mohammad Farahmand <mohammedfarahmand98@gmail.com>
2020-04-29 20:28:17 +0430

    Initial commit.

---
+++ app/nuxt.config.js
@@ -0,0 +1,21 @@
+export default {
+  mode: 'spa',
+  head: {
+    title: 'Amera',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+  },
+  loading: { color: '#ffa010', height: '5px' },
+  css: [],
+  plugins: [],
+  buildModules: ['@nuxtjs/eslint-module'],
+  modules: ['@nuxtjs/axios'],
+  axios: {},
+  build: {
+    extend(config, ctx) {}
+  },
+  generate: { dir: '../amera/built' }
+}
